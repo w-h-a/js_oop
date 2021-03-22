d617bc3cec5fe8a4259d36f4e1c220ff705adf31
Wes Anderson <wesley.herb.anderson@gmail.com>
2021-03-22 18:00:54 -0400

    adding banner to exercises

---
+++ exercises/banner.js
@@ -0,0 +1,28 @@
+class Banner {
+  constructor(message) {
+    this.message = message;
+  }
+
+  displayBanner() {
+    console.log([this.horizontalRule(), this.emptyLine(), this.messageLine(), this.emptyLine(), this.horizontalRule()].join("\n"));
+  }
+
+  horizontalRule() {
+    return `+${'-'.repeat(this.message.length + 2)}+`;
+  }
+
+  emptyLine() {
+    return `|${' '.repeat(this.message.length + 2)}|`;
+  }
+
+  messageLine() {
+    return `| ${this.message} |`;
+  }
+}
+
+
+let banner1 = new Banner('To boldly go where no one has gone before.');
+banner1.displayBanner();
+
+let banner2 = new Banner('');
+banner2.displayBanner();
